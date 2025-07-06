f597204fba992dcf05e6b8b3c31f440ba549af1e
dox435 <charliealt4545@gmail.com>
2025-07-06 07:15:03 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('theme-toggle');
-
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
-  toggle.textContent = document.body.classList.contains('light-mode') ? '🌙' : '☀️';
-});
