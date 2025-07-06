ae402cb77253cb20c7e975c2e56644339c7e9a78
dox435 <charliealt4545@gmail.com>
2025-07-05 19:40:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('theme-toggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+  toggle.textContent = document.body.classList.contains('light-mode') ? '🌙' : '☀️';
+});
